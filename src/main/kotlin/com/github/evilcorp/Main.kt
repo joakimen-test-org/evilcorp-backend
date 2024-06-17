6f167d4bfe52f3c329643d0393bbffe136bba229
Joakim L. Engeset <joakim.engeset@gmail.com>
2024-06-17 16:22:44 +0200

    Initial commit

---
+++ src/main/kotlin/com/github/evilcorp/Main.kt
@@ -0,0 +1,9 @@
+package com.github.evilcorp
+
+import mu.KotlinLogging
+
+private val log = KotlinLogging.logger {}
+
+fun main(args: Array<String>) {
+    log.info("Hello, World!")
+}
---
+++ src/test/kotlin/com/github/evilcorp/MainTest.kt
@@ -0,0 +1,31 @@
+package com.github.evilcorp
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+import java.nio.charset.StandardCharsets.UTF_8
+
+private fun captureStdout(callable: () -> Unit): String {
+    return System.out.let { out ->
+        val buffer = ByteArrayOutputStream()
+        try {
+            System.setOut(PrintStream(buffer, true, "UTF-8"))
+
+            callable()
+
+            return@let buffer.toByteArray().toString(UTF_8).trim()
+        } finally {
+            System.setOut(out)
+        }
+    }
+}
+
+class MainTest {
+
+    @Test
+    fun testMain() {
+        val actual = captureStdout { main(arrayOf()) }
+        assertThat(actual).endsWith(".Main -- Hello, World!")
+    }
+}
